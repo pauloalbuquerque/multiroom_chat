90c9e9c15e6783221e701e9810315427f850e2ef
pauloalbuquerque <pauloalbuquerque.paz@gmail.com>
2018-09-12 08:48:23 -0300

    'message'

---
+++ app.js
@@ -0,0 +1,20 @@
+/* importar as configurações do servidor */
+var app = require('./config/server');
+
+/* parametrizar porta de escuta */
+var server = app.listen(80, function(){
+    console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* Criar a conexão por WebSocket */
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    });
+});
